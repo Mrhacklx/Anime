b05fac8a78fd9062c954e839dbc1e812ba1a2b2c
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2023-10-21 10:17:58 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -336,8 +336,8 @@ async def start(client, message):
                 )
             )
             filesarr.append(msg)
-        k = await client.send_message(chat_id = message.from_user.id, text=f"<b><u>\n\nयह movie 30 sec में हटा दिया जाएगा \n\n इस movie को देखने के लिए जल्द से जल्द \n🧲𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗲 𝗗𝗶𝗿𝗲𝗰𝘁 𝗟𝗶𝗻𝗸✅ पर क्लिक करके लिंक को save कर ले \n\n Thank you❣️\n</u></b>")
-        await asyncio.sleep(30)
+        k = await client.send_message(chat_id = message.from_user.id, text=f"<b><u>\n\nयह movie 300 sec में हटा दिया जाएगा \n\n इस movie को देखने के लिए जल्द से जल्द \n🧲𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗲 𝗗𝗶𝗿𝗲𝗰𝘁 𝗟𝗶𝗻𝗸✅ पर क्लिक करके लिंक को save कर ले \n\n Thank you❣️\n</u></b>")
+        await asyncio.sleep(300)
         for x in filesarr:
             await x.delete()
         await k.edit_text("<b>Your All Files/Videos is successfully deleted!!!</b>")
@@ -408,8 +408,8 @@ async def start(client, message):
             btn = [[
                 InlineKeyboardButton("Get File Again", callback_data=f'delfile#{file_id}')
             ]]
-            k = await msg.reply("<b><u>\n\nयह movie 30 sec में हटा दिया जाएगा \n\n इस movie को देखने के लिए जल्द से जल्द \n🧲𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗲 𝗗𝗶𝗿𝗲𝗰𝘁 𝗟𝗶𝗻𝗸✅ पर क्लिक करके लिंक को save कर ले \n\n Thank you❣️\n</u></b>",quote=True)
-            await asyncio.sleep(30)
+            k = await msg.reply("<b><u>\n\nयह movie 300 sec में हटा दिया जाएगा \n\n इस movie को देखने के लिए जल्द से जल्द \n🧲𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗲 𝗗𝗶𝗿𝗲𝗰𝘁 𝗟𝗶𝗻𝗸✅ पर क्लिक करके लिंक को save कर ले \n\n Thank you❣️\n</u></b>",quote=True)
+            await asyncio.sleep(300)
             await msg.delete()
             await k.edit_text("<b>Your File/Video is deleted!!!\n\nClick below button to get your deleted file 👇</b>",reply_markup=InlineKeyboardMarkup(btn))
             return
@@ -454,8 +454,8 @@ async def start(client, message):
     btn = [[
         InlineKeyboardButton("Get File Again", callback_data=f'delfile#{file_id}')
     ]]
-    k = await msg.reply("<b><u>\n\nयह movie 30 sec में हटा दिया जाएगा \n\n इस movie को देखने के लिए जल्द से जल्द \n🧲𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗲 𝗗𝗶𝗿𝗲𝗰𝘁 𝗟𝗶𝗻𝗸✅ पर क्लिक करके लिंक को save कर ले \n\n Thank you❣️\n</u></b>",quote=True)
-    await asyncio.sleep(30)
+    k = await msg.reply("<b><u>\n\nयह movie 300 sec में हटा दिया जाएगा \n\n इस movie को देखने के लिए जल्द से जल्द \n🧲𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗲 𝗗𝗶𝗿𝗲𝗰𝘁 𝗟𝗶𝗻𝗸✅ पर क्लिक करके लिंक को save कर ले \n\n Thank you❣️\n</u></b>",quote=True)
+    await asyncio.sleep(300)
     await msg.delete()
     await k.edit_text("<b>Your File/Video is successfully deleted!!!\n\nClick below button to get your deleted file 👇</b>",reply_markup=InlineKeyboardMarkup(btn))
     return   
